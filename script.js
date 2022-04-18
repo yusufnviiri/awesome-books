251a8d6f403b5615bf83a010f857f80aacb9c273
yusufnviiri <yusufnviiri@gmail.com>
2022-04-18 17:11:49 +0300

    Add linter files and fix linter errors

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-let bookArray = [
-    {
-        author: 'Lorem Ipsum',
-        title: 'Testeroo Testyy'
-    },
-    {
-        author: 'Lorem Ipsum',
-        title: 'Testeroo Testyy'
-    },
-    {
-        author: 'Lorem Ipsum',
-        title: 'Testeroo Testyy'
-    }
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
+
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const bookArray = [
+  {
+    author: 'Lorem Ipsum',
+    title: 'Testeroo Testyy',
+  },
+  {
+    author: 'Lorem Ipsum',
+    title: 'Testeroo Testyy',
+  },
+  {
+    author: 'Lorem Ipsum',
+    title: 'Testeroo Testyy',
+  },
 ];
 
 function addBook(bookTitle, bookAuthor) {
-    bookArray.push(
-        {
-            author: bookAuthor,
-            title: bookTitle
-        }
-    );
+  bookArray.push({
+    author: bookAuthor,
+    title: bookTitle,
+  });
 }
 
-function removeBook() {
-
-}
+function removeBook() {}
 
 function displayBooks() {
-    console.log(bookArray);
+
 }
 
 const bookList = document.querySelector('.bookList');
@@ -36,8 +38,8 @@ const form = document.querySelector('form');
 // bookList.innerHTML = ``;
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    addBook(title.value, author.value);
+  e.preventDefault();
+  addBook(title.value, author.value);
 });
 
 displayBooks();
